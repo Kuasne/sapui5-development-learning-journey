1467f45bc4c09adcdc2926afc29ffafa89d6e22c
Kuasne <keybgaming.007@gmail.com>
2025-10-21 11:14:23 -0300

    att3

--- webapp/controller/index.js
+++ webapp/controller/index.js
@@ -1,7 +1,16 @@
-sap.ui.define(["sap/m/Text"], function (Text) {
+sap.ui.define(["sap/ui/core/mvc/XMLView", "sap/m/Text"], function (XMLView, Text) {
     "use strict";
-
+    
     new Text({
         text: "Hello, SAPUI5!"
     }).placeAt("app");
+
+    XMLView.create({
+        id: "App",
+        viewName: "sap.training.exc.view.App"
+    }).then(function (oView) {
+        oView.placeAt("content");
+    });
+    
 });
+
