30b36c92dab2dea0dfd274f8c79785c44e4e8f6a
Kuasne <keybgaming.007@gmail.com>
2025-10-22 10:43:50 -0300

    att

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,34 +6,33 @@ sap.ui.define([
 
     return Controller.extend("sap.training.exc.controller.App", {
         onInit: function () {
-
+            // Código de inicialização, se necessário
         },
 
         onItemSelect: function (oEvent) {
             var oItem = oEvent.getParameter("item");
             var sKey = oItem.getKey();
+            var sItemText = oItem.getText(); // Pega o texto do item clicado
 
-            switch (sKey) {
-                case "home":
-                    MessageToast.show("Página Home Selecionada");
-                    break;
-                case "customers":
-                    MessageToast.show("Página Clientes Selecionada");
-                    break;
-                case "settings":
-                    MessageToast.show("Configurações Selecionadas");
-                    break;
-                default:
-                    MessageToast.show("Item desconhecido selecionado"); 
-            }
+            // Mostra uma mensagem com o texto do item
+            MessageToast.show("Você clicou em: " + sItemText);
+
+            // Você pode adicionar lógica aqui para navegar para outras
+            // views ou carregar dados diferentes baseado no item clicado (sKey)
         }
+
+        // Não precisamos mais das funções onInputChange e onButtonPress
+        // porque removemos o Input e o Button da view App.view.xml.
+        // Se você os adicionar novamente, descomente/adicione as funções.
+        /*
+        onInputChange: function(oEvent){
+             var sInputValue = oEvent.getParameter("value");
+             console.log("Input mudou para: ", sInputValue);
+        },
+
+        onButtonPress: function(){
+             MessageToast.show("Botão clicado!");
+        }
+        */
     });
-});
-
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
-
-	return Controller.extend("sap.ui.comp.sample.smartfield.SmartFieldWithValueHelp.Main");
 });
